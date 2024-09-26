75fcc8deb320a2afbf65314255b2c6381fb010c6
rytst <r.saito0111@gmail.com>
2024-09-26 17:37:40 +0900

    add hello_syscall.asm

---
+++ section01/03_hello_world/hello_syscall.asm
@@ -0,0 +1,13 @@
+section .text
+global _start                    ; `global` directive in NASM specific
+_start:
+    mov rdx, 0xe                 ; length of writing data
+    mov rsi, msg                 ; first address of writing data
+    mov rdi, 0x1                 ; file descriptor
+    mov rax, 0x1                 ; write syscall
+    syscall                      ; calling syscall
+    mov rdi, 0x0                 ; exit status
+    mov rax, 0x3c                ; exit syscall
+    syscall                      ; calling syscall
+section .rodata
+    msg: db "Hello, World!", 0xa ; 0xa: new line code
