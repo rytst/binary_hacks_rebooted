3585a089a82db48ab55d1cd34c452ff7b2378fbc
rytst <r.saito0111@gmail.com>
2024-09-26 16:56:37 +0900

    modify syscall programs

--- section01/03_hello_world/hello_syscall_2.c
+++ section01/03_hello_world/hello_syscall_2.c
@@ -1,9 +1,10 @@
 // Hello, World! by syscall
-#include <sys/syscall.h>
+#include <sys/syscall.h> // for SYS_write constant
 #include <unistd.h>
 
 int main()
 {
+    // write(1, "Hello, World!\n", 14);
     syscall( SYS_write, 1, "Hello, World!\n", 14);
     return 0;
 }
